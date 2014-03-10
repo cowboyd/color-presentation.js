b14d623365e1dec84c8adb84117ebb1b8a2c021e
Charles Lowell <cowboyd@thefrontside.net>
2014-03-10 16:43:51 -0500

    make binding synchronous

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -1,7 +1,28 @@
-App = Ember.Application.createWithMixins({
-  color: Color.create(),
-  colorParser: function() {
-    return Color.Parser.create()
-  }.property()
+App = Ember.Application.create()
+
+App.ApplicationController = Ember.Controller.extend({
+  color: Color.fromRGB(),
+  swatchStyle: function() {
+    var rgb = this.get('color.rgb')
+    return "background-color: " + "rgb(" + [rgb.r,rgb.g,rgb.b].join(',') + ")"
+  }.property('color.rgb'),
+  syntax: function() {
+    return ColorSyntax.create()
+  }.property(),
+  syntax2: function() {
+    return ColorSyntax.create()
+  }.property(),
+  
+  setupBindings: function() {
+    Ember.bind(this, 'syntax.output', 'color')
+    Ember.bind(this, 'syntax2.output', 'color')
+  }.on('init'),
+
+  observeColor: function() {
+    console.log('color', this.get('color.rgb'))
+  }.observes("color"),
+  observeSyntaxOutput: function() {
+    console.log('syntax.output', this.get('syntax.output.rgb'))
+  }.observes("syntax.output.rgb")
 })
 
